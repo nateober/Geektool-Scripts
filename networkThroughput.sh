eb22950555935c9075e9313710d3f2e455351e33
Nate Ober <nate.ober@schools.utah.gov>
2012-04-20 22:10:37 -0600

    Variable name readability change. Moved line choice from tail to awk. Changed structure of output.

--- networkThroughput.sh
+++ networkThroughput.sh
@@ -3,8 +3,8 @@
 # Nate [dot] Ober [at] Gmail
 
 IFACE=`route -n get default | grep -o "interface: .*" | awk '{print $2}'`
-BYTES_START=`netstat -ibI "$IFACE" | tail -1 | awk '{print $7 " " $10}'`
+BYTES_START=`netstat -ibI "$IFACE" | awk 'NR==2{print $7 " " $10}'`
 sleep 1
-BYTES_STOP=`netstat -ibI "$IFACE" | tail -1 | awk '{print $7 " " $10}'`
-FLOW=`echo $BYTES_START $BYTES_STOP | awk '{ print ($3-$1)/1024 " " ($4-$2)/1024}'`
-echo "$FLOW" | awk '{printf "in:%.2f\nkbps out:%.2f", $1, $2}'
+BYTES_STOP=`netstat -ibI "$IFACE" | awk 'NR==2{print $7 " " $10}'`
+KBYTES_FLOW=`echo $BYTES_START $BYTES_STOP | awk '{ print ($3-$1)/1024 " " ($4-$2)/1024}'`
+echo "$KBYTES_FLOW" | awk '{printf "%.2f kbps.in\n%.2f kbps.out", $1, $2}'
