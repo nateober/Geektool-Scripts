20eef52570138393c2e02ebc534dc5b96749774e
Nate Ober <nateober@gmail.com>
2012-07-21 22:50:00 -0600

    First Attempt at an Android phone monitor for Geektool.

---
+++ android_phone.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Created By Nate Ober
+# Nate [dot] Ober [at] Gmail
+
+# In order for this script to function, you need to have adb installed and in your path.
+# You can get adb in the Android SDK
+# I source my .bash_profile script to get access to adb.
+
+source ~/.bash_profile
+adb devices | awk /"device$"/'{print "Android Phone Connected\nDevice ID: "$1}' && \
+adb shell dumpsys battery | grep level | awk -F ": " '{printf "Battery: %d%%\n", $2}' && \
+adb shell dumpsys cpuinfo | awk '/% TOTAL:/{printf "CPU Utilization: %s\n", $1}' && \
+adb shell netcfg | awk '!/0\.0\.0\.0/ && !/127\.0\.0/{printf "IP Address: %s", $3}'
+
