87005cef13612b92a1b5592d648474d07078b79c
Nate Ober <nateober@gmail.com>
2012-08-20 15:05:15 -0600

    Updated with device name and added capacity for the connection of multiple android devices.

--- android_phone.sh
+++ android_phone.sh
@@ -6,8 +6,13 @@
 # You can get adb in the Android SDK
 # I source my .bash_profile script to get access to adb.
 
-adb devices | awk /"device$"/'{print "Android Phone Connected\nDevice ID: "$1}' && \
-adb shell dumpsys battery | grep level | awk -F ": " '{printf "Battery: %d%%\n", $2}' && \
-adb shell dumpsys cpuinfo | awk '/% TOTAL:/{printf "CPU Utilization: %s\n", $1}' && \
-adb shell netcfg | awk '!/0\.0\.0\.0/ && !/127\.0\.0/{printf "IP Address: %s", $3}'
+for device in $(adb devices | grep "device$" | awk '{print $1}')
+do
+	adb -s $device shell cat /system/build.prop | grep "ro\.product\.model=" | sed "s/ro\.product\.model=//"
+	adb -s $device shell dumpsys battery | grep level | awk -F ": " '{printf "Battery: %d%%\n", $2}'
+	adb -s $device shell dumpsys cpuinfo | awk '/% TOTAL:/{printf "CPU Utilization: %s\n", $1}' 
+	adb -s $device shell netcfg | awk '!/0\.0\.0\.0/ && !/127\.0\.0/{printf "IP Address: %s", $3}'
+	echo 
+	echo
+done
 
