882a96f21079df8b953429d74244c09c65fc896d
Nate Ober <nateober@gmail.com>
2012-08-29 09:58:04 -0600

    Cleaned up redundant greps and seds where possible.

--- android_phone.sh
+++ android_phone.sh
@@ -6,13 +6,12 @@
 # You can get adb in the Android SDK
 # I source my .bash_profile script to get access to adb.
 
-for device in $(adb devices | grep "device$" | awk '{print $1}')
+for device in $(adb devices | awk '/device$/ {print $1}')
 do
-	adb -s $device shell cat /system/build.prop | grep "ro\.product\.model=" | sed "s/ro\.product\.model=//"
-	adb -s $device shell dumpsys battery | grep level | awk -F ": " '{printf "Battery: %d%%\n", $2}'
+	adb -s $device shell cat /system/build.prop | awk -F "=" '/ro\.product\.model/ {print $2}'
+	echo "Serial Number: $device"
+	adb -s $device shell dumpsys battery | awk -F ": " '/level/{printf "Battery: %d%%\n", $2}'
 	adb -s $device shell dumpsys cpuinfo | awk '/% TOTAL:/{printf "CPU Utilization: %s\n", $1}' 
-	adb -s $device shell netcfg | awk '!/0\.0\.0\.0/ && !/127\.0\.0/{printf "IP Address: %s", $3}'
+	adb -s $device shell netcfg | awk '!/0\.0\.0\.0/ && !/127\.0\.0/{printf "IP Address: %s", $3}' | sed "s/\/.*//"
 	echo 
-	echo
 done
-
