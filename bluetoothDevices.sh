4d9e31fe8e890bb75dba5b8fddfee0a35ed6b41b
Nate Ober <nateober@gmail.com>
2013-04-02 10:20:42 -0600

    Single line for bluetooth.

--- bluetoothDevices.sh
+++ bluetoothDevices.sh
@@ -2,7 +2,6 @@
 # Created By Nate Ober
 # Nate [dot] Ober [at] Gmail
 
-echo "Bluetooth "
-echo "Device | Battery Remaining"
+echo "Bluetooth Device | Battery Remaining"
 echo "--------------------------"
 ioreg -c IOAppleBluetoothHIDDriver | awk -F " = " '/"Product" = / {print $2"  |  "} /"BatteryPercent" = / {print $2"%"}' |  sed 'N;s/\n/ /;' | sed -e 's/"//g'
